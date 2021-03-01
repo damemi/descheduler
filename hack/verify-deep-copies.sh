b94539a24922b901aad12e5a80b5446e43504f7e
Pravar Agrawal <pravaag1@in.ibm.com>
2021-03-01 23:55:54 +0530

    update verify scripts for all generators

---
+++ hack/verify-deep-copies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
+DESCHEDULER_ROOT=$(dirname "${BASH_SOURCE}")/..
+_tmpdir="$(mktemp -d "${DESCHEDULER_ROOT}/_tmp/kube-verify.XXXXXX")"
+
+_deschedulertmp="${_tmpdir}"
+mkdir -p "${_deschedulertmp}"
+
+git archive --format=tar --prefix=descheduler/ "$(git write-tree)" | (cd "${_deschedulertmp}" && tar xf -)
+_deschedulertmp="${_deschedulertmp}/descheduler"
+
+cp -r _output "${_deschedulertmp}"
+
+pushd "${_deschedulertmp}" > /dev/null 2>&1
+hack/update-generated-deep-copies.sh
+popd > /dev/null 2>&1
+
+ret=0
+
+pushd "${DESCHEDULER_ROOT}" > /dev/null 2>&1
+if ! _out="${diff _output "${_deschedulertmp}/_output"}"; then
+    echo "Generated deep-copies output differs:" >&2
+    echo "${_out}" >&2
+else
+    echo "Generated deep-copies output remains unchanged"
+fi
+popd > /dev/null 2>&1
+
+echo "Generated Deep Copes verified."
